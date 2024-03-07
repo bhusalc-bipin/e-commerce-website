e634f37709040ff6bbdc74bcd9f87321d9c35568
Bipin Bhusal Chhetri <bhusalc.bipin@gmail.com>
2024-03-07 13:20:26 -0500

    Add product detail page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,11 +9,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.css";
 import App from "./App.jsx";
 import HomePage from "./pages/HomePage.jsx";
+import ProductPage from "./pages/ProductPage.jsx";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
             <Route index={true} path="/" element={<HomePage />} />
+            <Route path="/product/:id" element={<ProductPage />} />
         </Route>
     )
 );

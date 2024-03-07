d660631aeeebb60f425494ae1836d54654f93b4c
Bipin Bhusal Chhetri <bhusalc.bipin@gmail.com>
2024-03-07 12:36:50 -0500

    Add react router for home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,23 @@
 import ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.css";
 import App from "./App.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route index={true} path="/" element={<HomePage />} />
+        </Route>
+    )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <RouterProvider router={router} />
+);

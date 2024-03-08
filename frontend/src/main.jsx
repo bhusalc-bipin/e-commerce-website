fe472ddafc65228a2b68adfad1f4c1ebffd02d16
Bipin Bhusal Chhetri <bhusalc.bipin@gmail.com>
2024-03-08 10:34:06 -0500

    Add redux store and api slice

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import {
     Route,
     RouterProvider,
 } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.css";
@@ -12,6 +13,7 @@ import "./styles/main.css";
 import App from "./App.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import ProductPage from "./pages/ProductPage.jsx";
+import store from "./store.js";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -23,5 +25,7 @@ const router = createBrowserRouter(
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
 );
